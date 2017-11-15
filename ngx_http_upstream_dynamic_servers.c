9c3907a864ef31af118c2caf26b3ce7a8f13a47e
Hakan Kuecuekyilmaz <hakan@myracloud.com>
2017-11-15 20:28:00 +0100

    Added max_conns for nginx >= 1.11.5

--- ngx_http_upstream_dynamic_servers.c
+++ ngx_http_upstream_dynamic_servers.c
@@ -93,7 +93,11 @@ static char * ngx_http_upstream_dynamic_server_directive(ngx_conf_t *cf, ngx_com
     time_t                       fail_timeout;
     ngx_str_t                   *value, s;
     ngx_url_t                    u;
+#if nginx_version >= 1011005
+    ngx_int_t                    weight, max_conns, max_fails;
+#else
     ngx_int_t                    weight, max_fails;
+#endif
     ngx_uint_t                   i;
     ngx_http_upstream_server_t  *us;
 
@@ -117,6 +121,9 @@ static char * ngx_http_upstream_dynamic_server_directive(ngx_conf_t *cf, ngx_com
     value = cf->args->elts;
 
     weight = 1;
+#if nginx_version >= 1011005
+    max_conns = 0;
+#endif
     max_fails = 1;
     fail_timeout = 10;
 
@@ -136,6 +143,23 @@ static char * ngx_http_upstream_dynamic_server_directive(ngx_conf_t *cf, ngx_com
 
             continue;
         }
+
+#if nginx_version >= 1011005
+        if (ngx_strncmp(value[i].data, "max_conns=", 10) == 0) {
+
+            if (!(uscf->flags & NGX_HTTP_UPSTREAM_MAX_CONNS)) {
+                goto not_supported;
+            }
+
+            max_conns = ngx_atoi(&value[i].data[10], value[i].len - 10);
+
+            if (max_conns == NGX_ERROR) {
+                goto invalid;
+            }
+
+            continue;
+        }
+#endif
 
         if (ngx_strncmp(value[i].data, "max_fails=", 10) == 0) {
 
